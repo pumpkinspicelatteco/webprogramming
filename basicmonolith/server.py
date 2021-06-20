a2dbf4fc1033044cab8bbf7b0725088f0641d1b1
Juliana Hill <julianahill.dev@gmail.com>
2021-06-20 12:34:31 -0500

    feat: add flask app instructions

--- basicmonolith/server.py
+++ basicmonolith/server.py
@@ -2,6 +2,14 @@
 # $ django-admin startproject config .
 # that will generate the scaffolding for a standard project
 
+# for flask:
+# $ python3 -m venv venv
+# $ source venv/bin/activate
+# $ pip3 install -r requirements.txt
+# $ export FLASK_APP=server
+# $ export FLASK_ENV=development
+# $ flask run
+
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
